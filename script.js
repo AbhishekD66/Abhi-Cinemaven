c67bf7a5c32749cc9f30ac2f2afefe556404c3fa
addy <addy@123.gov>
2024-04-12 08:10:02 +0530

    max change

--- script.js
+++ script.js
@@ -5,9 +5,6 @@ document.getElementById("editProfileBtn").addEventListener("click", function () 
   }
 });
 
-
-
-// Function to handle editing location
 document.getElementById("editLocationBtn").addEventListener("click", function () {
   var newLocation = prompt("Enter new location:");
   if (newLocation !== null && newLocation !== "") {
@@ -15,46 +12,24 @@ document.getElementById("editLocationBtn").addEventListener("click", function ()
   }
 });
 
-// Function to handle editing movies
-var editButtons = document.querySelectorAll(".edit-movie-btn");
-editButtons.forEach(function (button) {
-  button.addEventListener("click", function () {
-    var movieTitle = this.parentNode.querySelector("h3").innerText;
-    var newTitle = prompt("Enter new title for " + movieTitle + ":");
-    if (newTitle !== null && newTitle !== "") {
-      this.parentNode.querySelector("h3").innerText = newTitle;
-    }
-  });
+document.getElementById("change-image-btn").addEventListener("click", function () {
+  document.getElementById('imageSelectionModal').style.display = "block";
 });
 
+document.getElementsByClassName("close")[0].addEventListener("click", function () {
+  document.getElementById('imageSelectionModal').style.display = "none";
+});
+window.onclick = function (event) {
+  if (event.target == document.getElementById('imageSelectionModal')) {
+    document.getElementById('imageSelectionModal').style.display = "none";
+  }
+};
 
-
-// Check if geolocation is supported by the browser
-if ("geolocation" in navigator) {
-  // Prompt user for permission to access their location
-  navigator.geolocation.getCurrentPosition(
-    // Success callback function
-    (position) => {
-      // Get the user's latitude and longitude coordinates
-      const lat = position.coords.latitude;
-      const lng = position.coords.longitude;
-
-      // Do something with the location data, e.g. display on a map
-      console.log(`Latitude: ${lat}, longitude: ${lng}`);
-    },
-    // Error callback function
-    (error) => {
-      // Handle errors, e.g. user denied location sharing permissions
-      console.error("Error getting user location:", error);
-    }
-  );
-} else {
-  // Geolocation is not supported by the browser
-  console.error("Geolocation is not supported by this browser.");
-}
-
-
-let locationUpdate = document.querySelector('#location');
-
-locationUpdate.innerHTML ="new loc";
-//console.log(locationUpdate);
+var imageOptions = document.querySelectorAll(".image-option");
+imageOptions.forEach(function (option) {
+  option.addEventListener("click", function () {
+    var newSrc = this.src;
+    document.getElementById("profile-image").src = newSrc;
+    document.getElementById('imageSelectionModal').style.display = "none";
+  });
+});
